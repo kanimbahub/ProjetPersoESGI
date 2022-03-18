69c385c5bffb5045102c889c58f5bdc21e9ba274
root <root@manager.kanimba.com>
2022-03-18 20:15:05 +0100

    Proposal of options

---
+++ script/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#####################################################################
+#                                                                   # 
+#     Description : déployement de mon environnement docker         #
+#                                                                   #
+#                                                                   #
+#                                                                   #
+#                                                                   #
+#####################################################################
+
+echo "
+
+Options : 
+        -  --Create :  Craete Containers
+
+        -  --Drop :    Drop containers
+
+        -  --Infos :   Infos containers caracteristic (IP, Name, User ..)
+
+        -  --Start :   Starting containers
+
+        -  --Ansible : Deplying ansible
+
+"
